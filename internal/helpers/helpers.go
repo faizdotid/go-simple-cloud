f113c8e8b1812ec0a5fb58b5a87730f93045646d
Faizal <faizalnurpratama76@gmail.com>
2024-09-09 19:36:49 +0700

    chore: handling error expires data

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,21 +1,24 @@
 package helpers
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
-func CreateExpirationTime(exp Expiration) int64 {
+func CreateExpirationTime(exp Expiration) (int64, error) {
 	now := time.Now().Unix()
 	switch exp {
 	case OneMinute:
-		return now + 60
+		return now + 60, nil
 	case OneHour:
-		return now + 3600
+		return now + 3600, nil
 	case OneDay:
-		return now + 86400
+		return now + 86400, nil
 	case OneWeek:
-		return now + 604800
+		return now + 604800, nil
 	case OneMonth:
-		return now + 2592000
+		return now + 2592000, nil
 	default:
-		return 0
+		return 0, errors.New("invalid expiration time")
 	}
 }
