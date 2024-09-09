83d5464d5c2b3f2b721ab30cec0847c7385bcc0b
Faizal <faizalnurpratama76@gmail.com>
2024-09-09 19:37:16 +0700

    chore: add debug logging for development mode

--- internal/services/database.go
+++ internal/services/database.go
@@ -6,13 +6,16 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	// "gorm.io/gorm/logger"
 )
 
 func NewDatabaseService() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 	db, err := gorm.Open(
 		mysql.Open(dsn),
-		&gorm.Config{},
+		&gorm.Config{
+			// Logger: logger.Default.LogMode(logger.Info), // Activate this when debugging
+		},
 	)
 	if err != nil {
 		return nil, err
